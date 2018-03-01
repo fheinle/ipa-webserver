edb08927516709608744485160c2221cf48f3b4d
Florian Heinle <launchpad@planet-tiax.de>
2018-03-01 10:11:46 +0100

    init

---
+++ manifests/init.pp
@@ -0,0 +1,52 @@
+# webserver
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include webserver
+class webserver (
+  $ssl_dir = '/etc/apache2/ssl',
+) {
+
+  class { 'apache':
+    default_mods      => false,
+    default_vhost     => false,
+    default_ssl_vhost => false,
+    mpm_module        => 'worker',
+    purge_configs     => true,
+  }
+
+  class { 'apache::mod::auth_kerb': }
+  class { 'apache::mod::authnz_pam': }
+
+  file { 'ssl directory':
+    ensure => 'directory',
+    path   => $ssl_dir,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0770',
+  }
+
+  file { 'pam_sssd_http':
+    ensure  => present,
+    path    => '/etc/pam.d/http',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => @(END)
+    auth    required pam_sss.so
+    account required pam_sss.so
+    | END
+  }
+  $all_vhosts = lookup('webserver::vhosts', Hash, 'hash')
+
+  $all_vhosts.each |$vhost| {
+    if $vhost['vhost_name'] == $facts['fqdn'] {
+      create_resources(webserver::vhost,
+      {$facts['fqdn'] => $vhost})
+    }
+  }
+}
+
---
+++ manifests/vhost.pp
@@ -0,0 +1,74 @@
+# webserver::vhost
+#
+# Create a new virtual host
+#
+# @summary This is a wrapper arround 'puppetlabs/apache' with krb and ssl from FreeIPA
+#
+# @example
+#   webserver::vhost { 'namevar': }
+define webserver::vhost (
+    $vhost_name    = $facts['fqdn'],
+    $docroot       = "/var/www/$vhost_name/html",
+    $ssl           = true,
+    $kerberos      = false,
+    $web_user      = 'www-data',
+    $default_vhost = false,
+    $ssl_cert_filename = "/etc/apache2/ssl/${vhost_name}.crt.crt",
+    $ssl_key_filename  = "/etc/apache2/ssl/${vhost_name}.crt.key"
+  ) {
+
+  $krb_auth_realm  = lookup('webserver::krb_auth_realm', String)
+  $krb_5keytab     = lookup('webserver::krb_5keytab', String)
+  $krb_servicename = lookup('webserver::krb_servicename', String, 'first', 'http')
+
+  exec { "Create document root ${docroot}":
+    creates => $docroot,
+    command => "/bin/mkdir -p ${docroot}",
+    cwd     => '/var/www/'
+  }  -> file { $docroot:
+    ensure  => directory,
+    owner   => $web_user,
+    group   => $web_user,
+    mode    => '0750',
+  }
+
+  apache::vhost { $vhost_name:
+    servername    => $vhost_name,
+    port          => '80',
+    docroot       => $docroot,
+    default_vhost => $default_vhost,
+    access_log    => true,
+  }
+
+  if $kerberos == true {
+    Apache::Vhost[$vhost_name] {
+      auth_kerb              => 'true',
+      krb_auth_realm         => $krb_auth_realm,
+      krb_5keytab            => $krb_5keytab,
+      krb_servicename        => $krb_servicename,
+      krb_local_user_mapping => 'on'
+    }
+  }
+
+  if $ssl == true {
+    ipa::sslcert { "${krb_servicename}/${facts['fqdn']}":
+      fname   => $ssl_cert_filename,
+      domain  => $facts['fqdn'],
+      service => $krb_servicename,
+    }
+    Apache::Vhost[$vhost_name] {
+      port              => '443',
+      ssl_protocol      => 'TLSv1.2',
+      ssl_cert_filename => $ssl_cert_filename,
+      ssl_key_filename  => $ssl_key_filename,
+    }
+    apache::vhost { "redirect_${vhost_name}":
+      ensure          => present,
+      servername      => $vhost_name,
+      port            => '80',
+      docroot         => $docroot,
+      redirect_status => 'permanent',
+      redirect_dest   => "https://${vhost_name}/",
+    }
+  }
+}
